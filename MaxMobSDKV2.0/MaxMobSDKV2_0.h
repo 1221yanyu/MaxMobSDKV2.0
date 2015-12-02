9079a805ffabd2ecbbbe1897cb297d7a799f9101
1221yanyu <jacob@jacobdemacintosh.local>
2015-12-02 15:04:50 +0800

    Initial Commit

---
+++ MaxMobSDKV2.0/MaxMobSDKV2_0.h
@@ -0,0 +1,13 @@
+//
+//  MaxMobSDKV2_0.h
+//  MaxMobSDKV2.0
+//
+//  Created by Jacob on 15/12/2.
+//  Copyright © 2015年 Jacob. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MaxMobSDKV2_0 : NSObject
+
+@end
---
+++ MaxMobSDKV2.0/MaxMobSDKV2_0.m
@@ -0,0 +1,13 @@
+//
+//  MaxMobSDKV2_0.m
+//  MaxMobSDKV2.0
+//
+//  Created by Jacob on 15/12/2.
+//  Copyright © 2015年 Jacob. All rights reserved.
+//
+
+#import "MaxMobSDKV2_0.h"
+
+@implementation MaxMobSDKV2_0
+
+@end
