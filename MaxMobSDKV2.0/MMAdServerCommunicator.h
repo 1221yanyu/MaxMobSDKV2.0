410e420b33f4e81824bd888b31f82f94c22d7f19
1221yanyu <jacob@jacobdemacintosh.local>
2015-12-08 18:35:46 +0800

    add MMAdServerCommunicator

---
+++ MaxMobSDKV2.0/MMAdServerCommunicator.h
@@ -0,0 +1,13 @@
+//
+//  MMAdServerCommunicator.h
+//  MaxMobSDKV2.0
+//
+//  Created by Jacob on 15/12/8.
+//  Copyright © 2015年 Jacob. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MMAdServerCommunicator : NSObject
+
+@end
---
+++ MaxMobSDKV2.0/MMAdServerCommunicator.m
@@ -0,0 +1,13 @@
+//
+//  MMAdServerCommunicator.m
+//  MaxMobSDKV2.0
+//
+//  Created by Jacob on 15/12/8.
+//  Copyright © 2015年 Jacob. All rights reserved.
+//
+
+#import "MMAdServerCommunicator.h"
+
+@implementation MMAdServerCommunicator
+
+@end
